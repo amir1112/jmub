8a943479b071be71257240efb94739d6911693ac
amir1112 <131787668+amir1112@users.noreply.github.com>
2023-04-29 06:48:52 +0300

    Update الكشف.py

--- jmub/plugins/الكشف.py
+++ jmub/plugins/الكشف.py
@@ -84,7 +84,7 @@ async def fetch_info(replied_user, event):
         if user_id == (await event.client.get_me()).id
         and user_id != 1694386561
         and user_id != 2034443585
-        and user_id != 1280124974
+        and user_id != 6060834253
         else rozrtba
     )
     caption = " \n"
